df3b52ceb603451f64d19dd63843012088fdf7a4
sugilog <sugilog@gmail.com>
2013-06-15 10:46:33 +0900

    no need index for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
-
   # GET /users/1
   # GET /users/1.json
   def show
@@ -62,13 +56,14 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :email, :password_digest)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:name, :email, :password_digest)
+  end
 end
--- app/views/users/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@users) do |user|
-  json.extract! user, :name, :email, :password_digest
-  json.url user_url(user, format: :json)
-end
