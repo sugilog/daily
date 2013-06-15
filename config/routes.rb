89f891349ae168f122c7ef77f68956a2c67f8f15
sugilog <sugilog@gmail.com>
2013-06-15 22:28:59 +0900

    bundle exec rails g scaffold Topic title:string description:text user_id:integer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Daily::Application.routes.draw do
-  resources :users
+  resources :users do
+    resources :topics
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
