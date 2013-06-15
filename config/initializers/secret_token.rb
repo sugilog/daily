6f060d58ad2305e21e84bd9d1467e1ce8a309c4e
sugilog <sugilog@gmail.com>
2013-06-15 09:16:04 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Daily::Application.config.secret_key_base = '1692677c997ddd37a87a87cc61a1bec7e36315fcab36e34b05e0eff35dd1fe6d987269b22c5eafb753af22819ee189aa91a394b75f4a2aacbb350ce35631ac3e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Daily::Application.config.session_store :cookie_store, key: '_daily_session'
