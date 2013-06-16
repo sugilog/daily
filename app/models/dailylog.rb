eca40a9d2508229a505c8cc54503035b3f059cf4
sugilog <sugilog@gmail.com>
2013-06-16 11:11:14 +0900

    bundle exec rails g bootstrap:themed Dailylogs

--- app/models/dailylog.rb
+++ app/models/dailylog.rb
@@ -1,4 +1,4 @@
 class Dailylog < ActiveRecord::Base
   belongs_to :topic
-  validates :logged_on, presence: true, :uniqueness: {scope: [:topic_id]}
+  validates :logged_on, presence: true, uniqueness: {scope: [:topic_id]}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130615132459) do
+ActiveRecord::Schema.define(version: 20130616015955) do
+
+  create_table "dailylogs", force: true do |t|
+    t.date     "logged_on",  null: false
+    t.integer  "score"
+    t.text     "memo"
+    t.integer  "topic_id",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "dailylogs", ["topic_id", "logged_on"], name: "index_dailylogs_on_topic_id_and_logged_on", unique: true
 
   create_table "topics", force: true do |t|
     t.string   "title",       null: false
