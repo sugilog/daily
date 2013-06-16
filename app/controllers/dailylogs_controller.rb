8342bc5bb5bb8aa704d9968d203840bd782e681a
sugilog <sugilog@gmail.com>
2013-06-16 16:21:40 +0900

    add new and edit for dailylogs

--- app/controllers/dailylogs_controller.rb
+++ app/controllers/dailylogs_controller.rb
@@ -1,10 +1,35 @@
 class DailylogsController < ApplicationController
   before_action :login_user?
   before_action :set_topic
-  # before_action :set_dailylog, only: [:edit, :update, :destroy]
+  before_action :set_dailylog, only: [:edit, :update, :destroy]
 
   def index
     @dailylogs = Dailylog.monthly(@topic, start_date)
+  end
+
+  def new
+    @dailylog = @topic.dailylogs.new(logged_on: params[:logged_on])
+  end
+
+  def edit
+  end
+
+  def create
+    @dailylog = @topic.dailylogs.new(dailylog_params)
+
+    if @dailylog.save
+      redirect_to user_topic_dailylogs_path(current_user, @topic)
+    else
+      render action: :new
+    end
+  end
+
+  def update
+    if @dailylog.update_attributes(dailylog_params)
+      redirect_to user_topic_dailylogs_path(current_user, @topic)
+    else
+      render action: :edit
+    end
   end
 
   private
@@ -13,13 +38,13 @@ class DailylogsController < ApplicationController
     @topic = current_user.topics.find(params[:topic_id])
   end
 
-  # def set_dailylog
-  #   @dailylog = @topic.dailylogs.find(params[:id])
-  # end
+  def set_dailylog
+    @dailylog = @topic.dailylogs.find(params[:id])
+  end
 
-  # def dailylog_params
-  #   params.require(:dailylog).permit(:logged_on, :score, :memo)
-  # end
+  def dailylog_params
+    params.require(:dailylog).permit(:logged_on, :score, :memo)
+  end
 
   def start_date
     @start_date ||=
--- test/controllers/dailylogs_controller_test.rb
+++ test/controllers/dailylogs_controller_test.rb
@@ -35,4 +35,91 @@ class DailylogsControllerTest < ActionController::TestCase
       get :index
     end
   end
+
+  test "should get new" do
+    get :new, topic_id: @topic
+    assert_response :success
+    assert_template :new
+    assert_nil assigns(:dailylog).logged_on
+  end
+
+  test "should not get new with invalid combination of user and topic" do
+    another_user = users(:two)
+    @request.session[:user_id] = another_user.id
+
+    assert_raise(ActiveRecord::RecordNotFound) do
+      get :new, topic_id: @topic
+    end
+  end
+
+  test "should get new with logged_on" do
+    get :new, topic_id: @topic, logged_on: "2013-01-01"
+    assert_response :success
+    assert_template :new
+    assert_equal Date.new(2013, 1, 1), assigns(:dailylog).logged_on
+  end
+
+  test "should get edit" do
+    get :edit, topic_id: @topic, id: @dailylog
+    assert_response :success
+    assert_template :edit
+    assert_not_nil assigns(:dailylog)
+  end
+
+  test "should not get edit with invalid combination of user and topic" do
+    another_user = users(:two)
+    @request.session[:user_id] = another_user.id
+
+    assert_raise(ActiveRecord::RecordNotFound) do
+      get :edit, topic_id: @topic, id: @dailylog
+    end
+  end
+
+  test "should create" do
+    assert_difference("Dailylog.count", 1) do
+      assert_difference("@topic.dailylogs.size", 1) do
+        post :create, topic_id: @topic, dailylog: {logged_on: "2013-01-01", score: 1, memo: nil}
+      end
+    end
+
+    assert_redirected_to user_topic_dailylogs_path(@user, @topic)
+  end
+
+  test "should not create with invalid params" do
+    assert_no_difference("Dailylog.count") do
+      assert_no_difference("@topic.dailylogs.size") do
+        post :create, topic_id: @topic, dailylog: {logged_on: nil, score: 1, memo: nil}
+      end
+    end
+
+    assert_response :success
+    assert_template :new
+  end
+
+  test "should update" do
+    assert_no_difference("Dailylog.count") do
+      assert_no_difference("@topic.dailylogs.size") do
+        patch :update, topic_id: @topic, id: @dailylog, dailylog: {logged_on: "2013-01-01", score: 1, memo: nil}
+      end
+    end
+
+    @dailylog.reload
+    assert_equal Date.new(2013, 1, 1), @dailylog.logged_on
+
+    assert_redirected_to user_topic_dailylogs_path(@user, @topic)
+  end
+
+  test "should not update" do
+    assert_no_difference("Dailylog.count") do
+      assert_no_difference("@topic.dailylogs.size") do
+        patch :update, topic_id: @topic, id: @dailylog, dailylog: {logged_on: nil, score: 1, memo: nil}
+      end
+    end
+
+    @dailylog.reload
+    assert_not_nil @dailylog.logged_on
+
+    assert_response :success
+    assert_template :edit
+  end
 end
