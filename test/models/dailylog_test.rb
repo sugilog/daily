29d1316ecfb87fcab2a451bb46adb5051889f9c1
sugilog <sugilog@gmail.com>
2013-06-16 12:13:19 +0900

    update model tests

--- test/models/dailylog_test.rb
+++ test/models/dailylog_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class DailylogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @topic = topics(:one)
+    @dailylog = dailylogs(:one)
+
+    @valid_attributes = {
+      topic_id:  @topic.id,
+      logged_on: Date.yesterday,
+      score:     12.3,
+      memo:      "test"
+    }
+  end
+
+  test "should create" do
+    dailylog = Dailylog.new(@valid_attributes)
+    assert_difference("Dailylog.count", 1) do
+      assert dailylog.save
+    end
+  end
+
+  test "should not create unless logged_on is present" do
+    dailylog = Dailylog.new(@valid_attributes.merge(logged_on: nil))
+    assert_no_difference("Dailylog.count") do
+      assert !dailylog.save
+      assert dailylog.errors[:logged_on].any?
+    end
+  end
+
+  test "should not create unless logged_on is uniqueness in topic" do
+    dailylog = Dailylog.new(@valid_attributes.merge(logged_on: @dailylog.logged_on))
+    assert_no_difference("Dailylog.count") do
+      assert !dailylog.save
+      assert dailylog.errors[:logged_on].any?
+    end
+  end
 end
--- test/models/topic_test.rb
+++ test/models/topic_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class TopicTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @topic = topics(:one)
+
+    @valid_attributes = {
+      user_id:     @user.id,
+      title:       "title",
+      description: "description"
+    }
+  end
+
+  test "should create" do
+    topic = Topic.new(@valid_attributes)
+    assert_difference("Topic.count", 1) do
+      assert topic.save
+    end
+  end
+
+  test "should not create unless logged_on is present" do
+    topic = Topic.new(@valid_attributes.merge(title: nil))
+    assert_no_difference("Topic.count") do
+      assert !topic.save
+      assert topic.errors[:title].any?
+    end
+  end
 end
